adfd71eafcdca10a60aefd47654f20b24ee1b570
sejalg1019 <73032519+sejalg1019@users.noreply.github.com>
2023-02-19 18:08:06 -0500

    updated

--- scraper.py
+++ scraper.py
@@ -6,12 +6,11 @@ url= "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_sta
 
 request= requests.get(url)
 
+soup = bs(request.text, "html.parser")
+star_data = soup.find("table")
+data = []
 
-soup= bs(request.text, "html.parser")
-star_data= soup.find("table")
-data=[]
-
-table_rows= star_data.find_all("tr")
+table_rows = star_data.find_all("tr")
 
 for tr in table_rows:
     table_data= tr.find_all("td")
@@ -19,10 +18,10 @@ for tr in table_rows:
     data.append(row)
 
 
-name=[]
-distance=[]
-mass=[]
-radius=[]
+name = []
+distance = []
+mass = []
+radius = []
 
 for i in range(1,len(data)):
     name.append(data[i][1])
@@ -31,7 +30,7 @@ for i in range(1,len(data)):
     radius.append(data[i][6])
 
 
-df= pd.DataFrame(list(zip(name, distance, mass, radius)),columns=["star_name", "star_distance", "star_mass", "star_radius"])
+df = pd.DataFrame(list(zip(name, distance, mass, radius)),columns=["star_name", "star_distance", "star_mass", "star_radius"])
 print(df)
 
 df.to_csv("star_data.csv")
